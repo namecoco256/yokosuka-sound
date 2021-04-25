756e71b880d605a89ee9a2b29f279fb3284da961
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2021-04-25 16:36:58 +0900

    Update scr.js

--- scr.js
+++ scr.js
@@ -13,6 +13,17 @@ function geoFindMe() {
     status.textContent = '';
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    
+    //追加部分
+    var reica_request = new XMLHttpRequest();
+    var reica_mapapi = "https://nominatim.openstreetmap.org/search?q=" + latitude + ","+ longitude + "&format=xml&polygon=1&addressdetails=1"
+    reica_request.open('GET', reica_mapapi, true);
+    reica_request.onload = function () {
+      var reica_data = this.response;
+      console.log(reica_data);
+    }
+    reica_request.send();
+    //ここまで追加部分
   }
 
   function error() {
