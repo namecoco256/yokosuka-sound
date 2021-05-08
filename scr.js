eebe346dce9fea40a8528f3366d991ab216e25fa
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2021-05-08 16:00:50 +0900

    Update scr.js

--- scr.js
+++ scr.js
@@ -7,19 +7,19 @@ const yn = document.getElementById("yn");
 var automode = false;
 function geoFindMe() {
 
-  const status = document.querySelector('#status');
+  /*const status = document.querySelector('#status');
   const mapLink = document.querySelector('#map-link');
 
   mapLink.href = '';
-  mapLink.textContent = '';
+  mapLink.textContent = '';*/
 
   function success(position) {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    status.textContent = '';
+    /*status.textContent = '';
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;*/
     
 
     var reica_request = new XMLHttpRequest();
@@ -42,11 +42,11 @@ function geoFindMe() {
   }
 
   function error() {
-    status.textContent = 'Unable to retrieve your location';
+    //status.textContent = 'Unable to retrieve your location';
   }
 
   if(!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
+    //status.textContent = 'Geolocation is not supported by your browser';
   } else {
     status.textContent = 'Locating…';
     navigator.geolocation.getCurrentPosition(success, error);
