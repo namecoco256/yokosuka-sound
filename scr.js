19a45f14ef23348da60b46021ea70e461cc8d1c3
namecoco256 <74481445+namecoco256@users.noreply.github.com>
2021-06-18 20:23:46 +0900

    Update scr.js

--- scr.js
+++ scr.js
@@ -4,7 +4,7 @@ const button = document.getElementById("button");
 //↑ボタン
 const yn = document.getElementById("yn");
 //↑横須賀にいるかどうか
-var yokosukaSE = new Audio("sounds/stupid5.mp3");
+var yokosukaSE = new Audio("sounds/stupid5.wav");
 var automode = false;
 var autorefresh;
 
